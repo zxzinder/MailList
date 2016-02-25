61300ac7a9019e19b460fad731d50b7329541c3b
zx <zxzinder@163.com>
2016-02-25 17:24:11 +0800

    Initial Commit

---
+++ MailList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MailList
+//
+//  Created by MacMini on 16/2/25.
+//  Copyright © 2016年 MacMini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
